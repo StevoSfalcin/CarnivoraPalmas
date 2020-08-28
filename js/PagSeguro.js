e073f8dcb93947a33a5474c1e92e236ae457e407
stevosfalcin <stevosfalcin@gmail.com>
2020-08-28 14:26:30 -0300

    :)

--- js/PagSeguro.js
+++ js/PagSeguro.js
@@ -40,17 +40,7 @@ function listarMeiosPag(){
     }
     });
 }
-//INDENTIFICADOR COM BASE NOS DADOS DO CLIENTE
-function senderHash(){
-    PagSeguroDirectPayment.onSenderHashReady(function(retorno){
-        if(retorno.status == 'error') {       
-            return false;
-        }
-        var hash = retorno.senderHash; 
-    });
-}
 //OBTER BANDEIRA DO CARTAO
-
 $('#numCartao').on('keyup', function () {
     var numCartao = $(this).val();
     var lenghtNum = numCartao.length;
@@ -59,7 +49,7 @@ $('#numCartao').on('keyup', function () {
             cardBin: numCartao,
             success: function(retorno) {
               var imgBand = retorno.brand.name;
-              recupParcelas(imgBand);
+              obterParcelas(imgBand);
             },
             error: function(retorno) {
               //tratamento do erro
@@ -70,8 +60,8 @@ $('#numCartao').on('keyup', function () {
         });
     }
 })
-
-function recupParcelas(imgBand){
+//OBTER PARCELAS
+function obterParcelas(imgBand){
     PagSeguroDirectPayment.getInstallments({
         amount: amount,
         maxInstallmentNoInterest: 2,
@@ -81,7 +71,7 @@ function recupParcelas(imgBand){
             $.each(retorno.installments, function (ia, obja) {
                 $.each(obja,function(ib,objb) {
                     var valorParcela = objb.installmentAmount.toFixed(2).replace(".",",");
-                   $('#qntParcelas').show().append("<option value='"+objb.quantity+"'data-parcelas='"+objb.installmentAmount+"'>"+objb.quantity+"Parcelas de R$"+valorParcela+"</option>");
+                   $('#qntParcelas').show().append("<option value='"+objb.quantity+"'data-parcelas='"+objb.installmentAmount+"'>"+objb.quantity+" Parcelas de R$"+valorParcela+"</option>");
                 });
             });
        },
@@ -97,6 +87,63 @@ function recupParcelas(imgBand){
 $('#qntParcelas').change(function () {
     $('#valorParcelas').val($('#qntParcelas').find(':selected').attr('data-parcelas'));
 });
+
+$("#formPagamento").on("submit", function (event) {
+    event.preventDefault();
+    PagSeguroDirectPayment.createCardToken({
+        cardNumber: $('#numCartao').val(),
+        brand: $('#bandeiraCartao').val(), 
+        cvv: $('#cvvCartao').val(), 
+        expirationMonth: $('#mesValidade').val(),
+        expirationYear: $('#anoValidade').val(),
+        success: function (retorno) {
+            $('#tokenCartao').val(retorno.card.token);
+        },
+        error: function (retorno) {
+            // Callback para chamadas que falharam.
+        },
+        complete: function (retorno) {
+            // Callback para todas chamadas.
+            hashCartao();
+        }
+    });
+})
+
+function hashCartao(){
+    PagSeguroDirectPayment.onSenderHashReady(function(response){
+        if(response.status == 'error') {
+            console.log(response.message);
+            return false;
+        }
+        var hash = response.senderHash; //Hash estará disponível nesta variável.
+        $('#hasCartao').val(retorno.senderHash);
+        var dados = ('#formPagamento').serialize();
+        console.log(dados);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
