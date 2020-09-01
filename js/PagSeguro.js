49de0ed420f2e6aadcb348c92e5159843fe4763b
stevosfalcin <stevosfalcin@gmail.com>
2020-09-01 08:01:04 -0300

    :)

--- js/PagSeguro.js
+++ js/PagSeguro.js
@@ -132,7 +132,7 @@ function hashCartao(){
                 data:dados,
                 dataType:"json",
                 success: function(retorno){
-                    console.log(retorno);
+                    console.log("sucesso"+JSON.stringify(retorna));
                 },
                 error:function(retorno){
                     console.log('erro')
