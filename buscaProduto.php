7af5895d433081c39d3af32219f00083cee5a0d8
stevosfalcin <stevosfalcin@gmail.com>
2020-09-04 14:49:41 -0300

    :)

--- buscaProduto.php
+++ buscaProduto.php
@@ -1,10 +1,10 @@
 <?php
-require_once 'config/config';
+require_once 'config/config.php';
 require_once 'App/lib/Database/Conexao.php';
 require_once 'App/Model/Produto.php';
 
     if(isset($_POST['palavra'])){
-    $dados = filter_var($_REQUEST['palavra'],FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $dados = filter_var($_POST['palavra'],FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $busca = \App\Model\Produto::pesquisa($dados);
 
     echo json_encode($busca);
