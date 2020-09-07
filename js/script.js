36cb877da282e75cfbda924d2ead53d4f1dde48f
stevosfalcin <stevosfalcin@gmail.com>
2020-09-07 20:16:20 -0300

    :)

--- js/script.js
+++ js/script.js
@@ -17,12 +17,13 @@
 
     //BUSCA PRODUTO
     $(document).ready(function(){
+        console.log($('input[name="volumeTotal"]').val());
         $('.frete input[name="cep"]').keyup(function(){
             var cep = $('input[name="cep"]').val();
             var dados = [];
             dados['cep'] = $('input[name="cep"]').val();
-            dados['volume'] = $('input[name="volumeTotal"]').val();
-            dados['peso'] = $('input[name="pesoTotal"]').val();
+            dados['volume'] = document.getElementsByClassName("volumeTotal")
+            dados['peso'] = $('input[name="pesoTotal"]').attr('value');
             if(cep.length >= 8){
                 $.ajax({
                     'url' : 'calculoFrete.php',
